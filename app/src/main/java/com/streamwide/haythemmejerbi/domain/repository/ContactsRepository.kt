c1584872353a828086696d3b13c682c83a872f82
Haythem <userthree@xtendplex.com>
2022-12-18 22:13:59 +0100

    Add domain layer

---
+++ app/src/main/java/com/streamwide/haythemmejerbi/domain/repository/ContactsRepository.kt
@@ -0,0 +1,10 @@
+package com.streamwide.haythemmejerbi.domain.repository
+
+import com.streamwide.haythemmejerbi.data.model.Contact
+import kotlinx.coroutines.flow.Flow
+
+interface ContactsRepository {
+    suspend fun fetchContacts(): Flow<List<Contact>>
+    fun searchBy(query: String): Flow<List<Contact>>
+    fun observeChanges(): Flow<Boolean>
+}
